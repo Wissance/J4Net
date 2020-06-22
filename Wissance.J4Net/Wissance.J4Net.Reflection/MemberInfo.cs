416d0524ccf7aefb001f97de4c8b14ac915e9c67
Ushakov Michael <um.nix.user@gmail.com>
2020-06-23 00:57:46 +0500

    member info all methods signature moved to new solution

--- Wissance.J4Net/Wissance.J4Net.Reflection/MemberInfo.cs
+++ Wissance.J4Net/Wissance.J4Net.Reflection/MemberInfo.cs
@@ -36,9 +36,11 @@
 */
 
 
+using System;
 using System.Collections.Generic;
 using System.Reflection;
 using Wissance.J4Net.Reflection.Attributes;
+using Wissance.J4Net.Reflection.Binders;
 
 namespace Wissance.J4Net.Reflection
 {
@@ -46,16 +48,83 @@ namespace Wissance.J4Net.Reflection
     {
         public bool IsDefined(Type attributeType, bool inherit)
         {
+            //return CustomAttributeData.__GetCustomAttributes(this, attributeType, inherit).Count != 0;
             throw new System.NotImplementedException();
         }
 
         public IList<CustomAttributeData> Provide(Type attributeType, bool inherit)
         {
+            // return CustomAttributeData.__GetCustomAttributes(this, attributeType, inherit);
             throw new System.NotImplementedException();
         }
+        
+        public IList<CustomAttributeData> GetCustomAttributesData()
+        {
+            throw new System.NotImplementedException();
+            //return CustomAttributeData.GetCustomAttributes(this);
+        }
+
+        public IEnumerable<CustomAttributeData> CustomAttributes
+        {
+            get { return GetCustomAttributesData(); }
+        }
+
+        internal abstract bool BindingFlagsMatch(BindingFlags flags);
+
+        internal abstract bool BindingFlagsMatchInherited(BindingFlags flags);
+        
+        protected static bool BindingFlagsMatch(bool state, BindingFlags flags, BindingFlags trueFlag, BindingFlags falseFlag)
+        {
+            return (state && (flags & trueFlag) == trueFlag)
+                   || (!state && (flags & falseFlag) == falseFlag);
+        }
+
+        protected static T SetReflectedType<T>(T member, Type type)
+            where T : MemberInfo
+        {
+            return member == null ? null : (T)member.SetReflectedType(type);
+        }
+
+        protected static T[] SetReflectedType<T>(T[] members, Type type)
+            where T : MemberInfo
+        {
+            for (int i = 0; i < members.Length; i++)
+            {
+                members[i] = SetReflectedType(members[i], type);
+            }
+            return members;
+        }
+        
+        public static bool operator ==(MemberInfo m1, MemberInfo m2)
+        {
+            return ReferenceEquals(m1, m2) || (!ReferenceEquals(m1, null) && m1.Equals(m2));
+        }
+
+        public static bool operator !=(MemberInfo m1, MemberInfo m2)
+        {
+            return !(m1 == m2);
+        }
+        
+        internal abstract MemberInfo SetReflectedType(Type type);
         
         public string Name { get; internal set; }
         public Type DeclaringType { get; internal set; }
         //public abstract MemberTypes MemberType { get; }
+        public int MetadataToken { get; private set; }
+        
+        /// <summary>
+        ///    TODO: UMV: not transferred yet, should be another module
+        /// </summary>
+        public Module Module { get; private set; }
+        /// <summary>
+        ///    Was __IsMissing
+        /// </summary>
+        public bool IsMissing { get; private set; }
+        internal bool IsBaked { get; private set; }
+
+        public virtual Type ReflectedType
+        {
+            get { return DeclaringType; }
+        }
     }
 }
